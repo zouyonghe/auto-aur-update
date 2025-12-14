9f67106fa35b0b6ff77f30bb9f3dc9d9eb95cf32
ZouYonghe <1259085392z@gmail.com>
2025-12-15 03:15:23 +0900

    Fix makepkg permission issue by running as builder user

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -3,14 +3,15 @@
 pkgdir=$1
 pkgname=$2
 
-
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+chown -R builder:builder .
 chmod -R a+rw .
 
 pacman-key --init
 pacman -Sy --noconfirm &&
 
-bash scripts/${pkgname}.sh
+# 切换到 builder 用户执行脚本
+su - builder -c "cd /github/workspace && bash scripts/${pkgname}.sh"
 
 #echo OK
